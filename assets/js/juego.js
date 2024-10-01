96172bfd5a2642d4e21ccde4768ded2e0885d4f0
Jorge Alejandro Arias <ariaz39@hotmail.com>
2024-10-01 17:53:23 -0500

    :bento: :heavy_plus_sign: se agrego dependencia de underscore y se inicializo el deck

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,31 @@
-console.log('llega');
+/*
+2C = Two of  Clubs (Treboles)
+2D = Two of  Diamonds (Diamantes)
+2H = Two of  Hearts (Corazones)
+2S = Two of  Spades (Espadas)
+ */
+
+let deck = [];
+const types = ['C', 'D', 'H', 'S'];
+const especials = ['A', 'J', 'Q', 'K'];
+
+const createDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let type of types) {
+            deck.push(i + type);
+        }
+    }
+
+    for (let type of types) {
+        for (let esp of especials) {
+            deck.push(esp + type);
+        }
+    }
+
+    console.log(deck);
+    deck = _.shuffle(deck);
+
+    console.log(deck);
+}
+
+createDeck();
